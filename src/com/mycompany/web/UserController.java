519dd4698d3c89466bb094e596c00c4bd08f763b
yu_feng <yu_feng@chexiang.com>
2016-02-25 16:24:31 +0800

    userController

--- src/com/mycompany/web/UserController.java
+++ src/com/mycompany/web/UserController.java
@@ -9,4 +9,8 @@ public class UserController {
 	public void getUser(){
 		System.out.println("user...hello world");
 	}
+	
+	public static void main(String[] args){
+		System.out.println("userController go up! run");
+	}
 }
